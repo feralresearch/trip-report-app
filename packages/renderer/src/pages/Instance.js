165dafa836aba23d10f006a5e3534182eb2155f5
andrew <a@feralresearch.org>
2023-04-18 02:30:48 +0200

    Remove spinner

--- packages/renderer/src/pages/Instance.js
+++ packages/renderer/src/pages/Instance.js
@@ -7,26 +7,17 @@ import MediaList from "../components/MediaList";
 import styles from "./styles";
 import Spinner from "../components/Spinner";
 
-let timer;
-
 const Instance = () => {
   const params = useParams();
-  //const instanceId = params.id;
   const [data, setData] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    timer = setTimeout(() => setIsLoading(true), 250);
     const fetchData = async () => {
       const data = await window.databaseAPI.instanceGet(params.id);
       setData(data);
-      clearTimeout(timer);
-      setIsLoading(false);
     };
     fetchData();
   }, [params.id]);
-
-  if (isLoading || !data) return <Spinner showPercentage={false} />;
 
   const info = data?.filter((datum) => datum.tag === "world_enter")[0]?.data;
   const players = data?.filter((datum) => datum.tag === "player");
